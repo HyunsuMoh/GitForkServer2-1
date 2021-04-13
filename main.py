d0c97969911c6e11b72435873d77bfbf30a9a0f5
Inhye <sadnody@naver.com>
2021-04-13 19:32:47 +0900

    add Hello World 2 in team leader server

--- main.py
+++ main.py
@@ -3,4 +3,8 @@
 #
 print("Hello World 1 in team leader server")
 
+<<<<<<< HEAD
 print("Hello world 3 in team member C local server")
+=======
+print("Hello World 2 in team leader server")
+>>>>>>> add Hello World 2 in team leader server
